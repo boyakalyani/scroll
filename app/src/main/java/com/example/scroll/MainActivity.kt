2198c54944157e5e28aa9bf7c802b73f58f82833
kalyani <kalyani143jk@gmail.com>
2023-02-23 22:41:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scroll/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.scroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("b","loky")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("a","kalynia")
+    }
+}
